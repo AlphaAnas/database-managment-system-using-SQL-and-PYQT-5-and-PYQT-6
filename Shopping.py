807879dc71e381b1ea8d9069cc783add9a5a07f3
Alpha-Anas <muhd.anas70pe@gmail.com>
2023-12-06 23:40:10 +0500

    made shopping cart

---
+++ Shopping.py
@@ -0,0 +1,131 @@
+# Importing essential modules
+from PyQt6 import QtWidgets, uic, QtCore
+from PyQt6.QtCore import Qt
+from PyQt6.QtCore import QDate
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QHeaderView, QPushButton, QLineEdit,QMessageBox
+import sys
+import connection_string
+import Payment
+import pyodbc
+
+# # Replace these with your own database connection details
+server = 'DESKTOP-6367D0S'
+database = 'POSHAAK'  # Name of your Northwind database
+use_windows_authentication = False  # Set to True to use Windows Authentication
+username = 'sa'  # Specify a username if not using Windows Authentication
+password = 'anasking'  # Specify a password if not using Windows Authentication
+
+
+# # Create the connection string based on the authentication method chosen
+if use_windows_authentication:
+    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
+else:
+    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+class Shopping(QtWidgets.QMainWindow):
+    def __init__(self):
+         # Call the inherited classes __init__ method
+        super(Shopping, self).__init__() 
+        # Load the .ui file
+        uic.loadUi('Shopping.ui', self) 
+        
+        connection = pyodbc.connect(connection_string)
+
+        cursor = connection.cursor()
+
+                # TODO: Write SQL query to fetch orders data
+        
+        cursor.execute("select count(id) as number from products; " )
+        number = cursor.fetchone()
+        rows = number[0] #store the total number of rows to display
+        
+        cursor.execute("select* from products;")
+        products_data = cursor.fetchall()
+        print(products_data) # a list of tuples containing all the 
+        
+       
+       
+        # populating the cart
+        self.itemWidget.setRowCount(rows)
+        for i, row_data in enumerate(products_data):
+                for j, data in enumerate(row_data):
+                        item = QtWidgets.QTableWidgetItem(str(data))  # Convert data to string and create a QTableWidgetItem
+                        if j != 8:
+                            self.itemWidget.setItem(i, j, item)  # Set the item in the QTableWidget
+
+                        # Make the items non-editable
+                            item.setFlags(Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable) 
+       
+            
+                # Spread columns to fill the entire widget
+
+        header = self.itemWidget.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.Interactive) 
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch) #description wale ko column ko stretch kro
+        header.setSectionResizeMode(4, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(6,QHeaderView.ResizeMode.ResizeToContents)
+        
+        
+        cursor.execute("SELECT SUM(total - discount) AS total_bill_amount FROM cart c")
+        
+        
+        # Fetch the result and store it in a variable
+        total_bill_result = cursor.fetchone()
+        total_bill_amount = total_bill_result[0] 
+        self.total.setText(str(total_bill_amount))
+        
+        
+             
+        # funcionality of delete and back buttons
+        
+        self.backButton.clicked.connect(self.back)
+        self.deleteButton.clicked.connect(self.delete)
+        
+        
+        # checkout and continue button functionality
+        
+        self.continueButton.clicked.connect(self.customerWindow)
+        self.checkoutButton.clicked.connect(self.paymentWindow)
+            
+        connection.commit()
+        
+   
+        
+    def back(self):
+        self.hide()
+        
+    def delete(self):
+        flag =False
+        flag1 = self.show_warning("Are you sure you want to delete this item ?",flag)
+        if flag1:
+            
+            self.itemWidget.setRowHidden(self.i, True)
+            
+            # proceed to payment window
+    def paymentWindow(self):
+        
+        self.signup1 = Payment.MakePayment()
+        self.signup1.show()
+        
+    def customerWindow(self):
+          uic.loadUi("newScreen.ui",self)
+        
+    
+    
+        
+    
+        
+
+        
+        
+def main(): 
+    app = QApplication(sys.argv)
+    window = Shopping()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
